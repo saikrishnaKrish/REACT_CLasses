689f4f31c032b4c12e313c07337237a4fccdc944
saikrishna <saikrishna.kanteti@gmail.com>
2024-06-20 12:26:50 +0530

    updated pdf

---
+++ Hw.js
@@ -0,0 +1,72 @@
+// /*console.log("Hello Students");
+// console.log("Hello Students");
+// console.log("Hello Students");
+// console.log("Hello Students");
+// console.log("Hello Students");
+// console.log("Hello Students");
+// */
+
+
+
+// // =================================
+
+
+// // Variable Declarations
+
+// // -- a name giving to something
+
+
+// // let / var/ const 
+
+// let a =20
+// // leftside --> variable Name
+// // RightSide ---> Value
+
+// var a=220
+// let aa=223
+// const aaab=225
+// let value222
+// console.log(a)
+// console.log(aa)
+// console.log(aaab)
+
+// console.log(value222)
+
+
+
+function printName(){
+    
+    if(true){
+        var a1=22
+        let a2=224
+        const a3=34567
+    }
+
+    var a1=345
+    let a2=224
+
+console.log("Hi am from a Js prgram",a1)
+}
+
+
+// console.log(a1)
+
+printName()
+
+
+
+
+let a2=224
+
+ a2=222
+
+console.log(a2)
+
+const a3=45678
+const a4="sai krishna"
+console.log(a3)
+
+console.log(typeof a4)
+
+
+
---
+++ IntroToJS.js
@@ -0,0 +1,148 @@
+//19th and 20th June Notes
+
+// /*console.log("Hello Students");
+// console.log("Hello Students");
+// console.log("Hello Students");
+// console.log("Hello Students");
+// console.log("Hello Students");
+// console.log("Hello Students");
+// */
+
+
+
+// // =================================
+
+
+// // Variable Declarations
+
+// // -- a name giving to something
+
+
+// // let / var/ const 
+
+// let a =20
+// // leftside --> variable Name
+// // RightSide ---> Value
+
+// var a=220
+// let aa=223
+// const aaab=225
+// let value222
+// console.log(a)
+// console.log(aa)
+// console.log(aaab)
+
+// console.log(value222)
+
+
+
+function printName(){
+    
+    if(true){
+        var a1=22
+        let a2=224
+        const a3=34567
+    }
+
+    var a1=345
+    let a2=224
+
+console.log("Hi am from a Js prgram",a1)
+}
+
+
+// console.log(a1)
+
+printName()
+
+
+
+
+let a2=224
+
+ a2=222
+
+console.log(a2)
+
+const a3=45678
+const a4="sai krishna"
+console.log(a3)
+
+console.log(typeof a4)
+
+
+
+// primitive dataTypes
+// non-primitive dataTypes
+
+
+//primitive
+let name="Sirish"// data
+let num=203
+let bool=true
+
+// non primitive
+let inpArray=[2,24,5,6] //--1mb
+// 50 mb
+// out of space
+let inpArray1=["sa","ss","w33"]
+let obj={
+        name:"Sirish",
+        place:"Hyderbad"
+}
+
+
+// DataTypes
+// Bool
+// String
+// Number
+// Undefined
+// Null
+// Symbol
+
+console.log(typeof 8)
+console.log(typeof "sai")
+
+console.log( typeof "566")
+
+let a=123;
+console.log( typeof a)
+let name1="Sirish";
+console.log(typeof name1)
+let isPresent =true;
+console.log(typeof isPresent)
+let randomValue
+console.log(typeof randomValue)
+
+console.log(first)
+
+
+let const=7777
+console.log(const)
+
+
+
+// functions
+    function welcomeUser(){
+        console.log("welcome to Sirish Technologies")
+        return ""
+    }
+
+    console.log(welcomeUser())
+    console.log(welcomeUser())
+    console.log(welcomeUser())
+    console.log(welcomeUser())
+
+    function addNum(){
+        return 99
+    }
+
+    console.log(addNum())
+
+
+
+
+
+
+
+
